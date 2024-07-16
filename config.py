0e56b29e7c6e0bd5fed96e90b25f7f82abe5367f
Akshat164634 <73011075+Akshat164634@users.noreply.github.com>
2024-07-16 20:13:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7372393896:AAHyFVK4T59J5K79GEUmeOo_0UvAU4sQKoI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("HARSH")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1844756562")
 
 
 
